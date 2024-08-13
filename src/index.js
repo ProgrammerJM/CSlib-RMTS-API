dcfbc5575ddfd7c5e19131c8a2689d293dd0afb6
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-13 17:09:47 +0800

    feat: Add API endpoint to get all users
    This commit adds a new API endpoint `/api/getUsers` to retrieve all users from the database. It includes the necessary changes to the `index.js` file to register the new route. The `GetUsers` route file is also added to handle the request and return the users as a JSON response.
    
    This feature enhances the API by providing a way to fetch all users, which can be useful for administrative purposes or displaying a list of users in the frontend.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 const userRoute = require("./api/routes/User");
+const getUsers = require("./api/routes/GetUsers");
 
 app.use("/api/user", userRoute);
+app.use("/api/getUsers", getUsers);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
